63ddb9bcb089a4320ef2a451837b7a395768cd89
Ryan Hageman <improv42@gmail.com>
2015-11-18 22:02:44 -0600

    Update product index to panels, move delete to show view, assign missing image

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,6 @@ class Product < ActiveRecord::Base
   belongs_to :category
   belongs_to :brand
 
-  has_attached_file :avatar, styles: { large: "500x500#", medium: "300x300#", thumb: "100x100#" }, default_url: ":style_missing.png"
+  has_attached_file :avatar, styles: { large: "500x500#", medium: "300x300#", thumb: "100x100#" }, default_url: ':style_missing.jpg'
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
