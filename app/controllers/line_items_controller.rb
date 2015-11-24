a760fcb62e42ac2a40f72eeaebc27ac133d7b131
Ryan Hageman <improv42@gmail.com>
2015-11-24 09:16:50 -0600

    Add update quantity with current cart items

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -26,9 +26,8 @@ class LineItemsController < ApplicationController
   # POST /line_items
   # POST /line_items.json
   def create
-    product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(product: product, price: product.current_price)
-
+    # product = Product.find(params[:product_id])
+    @line_item = @cart.add_product(params[:product_id])
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to @line_item.cart, notice: 'Line item was successfully created.' }
